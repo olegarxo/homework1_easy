f5d76bb78af0a7b75bdcbc4c17a1bc42e1dfa5de
oleg <olegarxo15@gmail.com>
2022-06-24 14:18:26 +0300

    Сделал первое задание. В коментах первые наработки, хотел сначала только через while

---
+++ homeworke_1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число!");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if (number >=0)
+{
+
+   while (count < number)
+   {
+    Console.Write("1 ");
+    count+=1;
+    }
+
+}
+else {
+
+Console.WriteLine("Вы ввели отрицательное число");
+
+}
+/*Console.Clear();
+Console.WriteLine("Введите число!");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (count < number){
+
+    Console.WriteLine("1 ");
+    count+=1;
+}*/
