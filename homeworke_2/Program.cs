e45bee4e38e89137354cee3f4b90d4e9ee86f006
oleg <olegarxo15@gmail.com>
2022-06-24 14:59:50 +0300

    Сделал второе задание

---
+++ homeworke_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+double numberOne = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Второе число");
+double numberTwo = Convert.ToDouble(Console.ReadLine());
+if (numberOne < numberTwo)
+{
+    double tempy = numberOne / numberTwo;
+    Console.WriteLine($"Первое число поместиться {tempy} раз во втором числе");
+}
+else {
+
+Console.WriteLine("Второе число всеже больше первого");
+
+}
