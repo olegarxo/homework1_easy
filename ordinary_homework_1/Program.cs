33bef227382bf57610268ee15c11354d3443a0a0
oleg <olegarxo15@gmail.com>
2022-06-25 12:02:16 +0300

    Сделал первое дз

---
+++ ordinary_homework_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число!");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число!");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (numberOne > numberTwo){
+    max = numberOne;   
+}
+else if (numberOne > numberTwo){
+    max = numberTwo;   
+}
+Console.WriteLine($"Первое число {numberOne}, второе число {numberTwo} -> max={max} ");
