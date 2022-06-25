16c647b4ceddb530a6a0bdd0d32cddfecf2976c5
oleg <olegarxo15@gmail.com>
2022-06-25 14:32:11 +0300

    Сделал второе задание двумя способами, как ни странно через массив быстрее додумался как сделать)

---
+++ ordinary_homework_2/Program.cs
@@ -0,0 +1,44 @@
+﻿//Первое решение
+Console.WriteLine("Введите первое число!");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число!");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число!");
+int numberTree = Convert.ToInt32(Console.ReadLine());
+int max = numberOne;
+if (max < numberTwo){
+    max = numberTwo;
+    if (max < numberTree){
+        max = numberTree;
+    }
+}
+//else if (max < numberTree);
+   
+
+
+    Console.WriteLine(max);
+/*Второе решение
+Console.Clear();
+Console.WriteLine("Введите первое число!");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число!");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число!");
+int numberTree = Convert.ToInt32(Console.ReadLine());
+int[] max = {numberOne, numberTwo, numberTree};
+int tamp;
+for (int i=0; i<max.Length - 1; i++)
+{
+    for(int j=0; j<max.Length; j++)
+    {
+
+        if (max[i]>max[j])
+        {
+            tamp = max[i];
+            max[i] = max[j];
+            max[j] = tamp;
+        }
+    }
+}
+Console.WriteLine($"Первое число {numberOne}, второе число {numberTwo}, третье число {numberTree} -> max={max[0]} ");
+*/
