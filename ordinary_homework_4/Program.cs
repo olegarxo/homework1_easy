3bfd7884be066cab04db96b722a7e49203ae0605
oleg <olegarxo15@gmail.com>
2022-06-25 15:10:59 +0300

    Сделал четвертое дз проверьте пожалуйста первый вариант, не пойму в чем проблема что за ошибка

---
+++ ordinary_homework_4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int [] tamp = { };
+for (int i = 0; i <= number; i++){
+    tamp[i] = 0 + i;
+}
+for (int j = 0; j <= number; j++){
+    if (tamp[j] %2 ==1 )
+    Console.WriteLine(tamp[j]);
+}
+*/
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 ==0){
+    for (int i = 2; i <= number;i+=2)
+    Console.Write($"{i },");
+}
+else{
+    for (int j = 1; j <= number;j+=2)
+    Console.Write($"{j },");
+}
