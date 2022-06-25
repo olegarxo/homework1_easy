ed49754a61cb7b064c221285e2f3c9e0c2cc12f6
oleg <olegarxo15@gmail.com>
2022-06-25 14:41:18 +0300

    Сделал третье задание

---
+++ ordinary_homework_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number %2 == 0){
+    Console.WriteLine("Число четное");
+}
+else {
+    Console.WriteLine("Число нечетное");
+}
